315cba074776c9178e793900b2ca9de7b7c56e52
Máté Dombóvári <63262118+Stalker9651@users.noreply.github.com>
2024-11-26 09:31:08 +0100

    Add project files.

---
+++ VarosokDoga/Nagyvaros.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarosokDoga
+{
+    internal class Nagyvaros
+    {
+        public string Varos { get; set; }
+        public string Orszag { get; set; }
+        public float Nepesseg { get; set; }
+
+        public Nagyvaros(string line)
+        {
+            var split = line.Split(';');
+            Varos = split[0];
+            Orszag = split[1];
+            Nepesseg = float.Parse(split[2]);
+        }
+        override public string ToString()
+        {
+            return $"{Varos} ({Orszag}) - {Nepesseg:.00} millió fő";
+        }
+
+    }
+}
---
+++ VarosokDoga/Program.cs
@@ -0,0 +1,38 @@
+﻿using VarosokDoga;
+
+List<Nagyvaros> nagyvarosok = [];
+using(var sr = new StreamReader(@"..\..\..\src\varosok.csv"))
+{
+	_ = sr.ReadLine();
+    while (!sr.EndOfStream)
+	{
+		nagyvarosok.Add(new Nagyvaros(sr.ReadLine()));
+    }
+}
+Console.WriteLine($"Kollekció hossza: {nagyvarosok.Count}");
+
+//1. feladat
+Console.WriteLine($"1. feladat: {nagyvarosok.Where(x=>x.Orszag == "Kína").Sum(y=>y.Nepesseg):.00}");
+
+//2. feladat
+Console.WriteLine($"2. feladat: {nagyvarosok.Where(x=>x.Orszag=="India").Average(y=>y.Nepesseg):.00}");
+
+//3. feladat
+Console.WriteLine($"3. feladat: {nagyvarosok.MaxBy(x=>x.Nepesseg).ToString()}");
+
+// 4.feladat
+var f4 = nagyvarosok.Where(x => x.Nepesseg > 20).OrderByDescending(y => y.Nepesseg);
+Console.WriteLine($"4. feladat:");
+foreach (var item in f4)
+{
+    Console.WriteLine(item.ToString());
+}
+
+//5. feladat
+Console.WriteLine($"5. feladat: {nagyvarosok.GroupBy(x => x.Orszag).Count(y => y.Count() > 1)}");
+
+//6. feladat
+Console.WriteLine($"6. feladat: {nagyvarosok.GroupBy(x => x.Varos[0]).MaxBy(y => y.Count()).Key}");
+
+
+
